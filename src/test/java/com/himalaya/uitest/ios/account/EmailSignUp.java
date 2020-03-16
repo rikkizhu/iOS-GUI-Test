c8e94b07e86f59fd9fce63c87f8ee62370cdea8b
xmly <rachel.zhu@ximalaya.com>
2020-03-16 17:10:06 +0800

    增加邮箱注册用例

---
+++ src/test/java/com/himalaya/uitest/ios/account/EmailSignUp.java
@@ -0,0 +1,54 @@
+package com.***REMOVED***.uitest.ios.account;
+
+import com.***REMOVED***.uitest.ios.AbstractTestCase;
+import com.***REMOVED***.uitest.ios.Utils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EmailSignUp extends AbstractTestCase {
+
+    @Test(description = "邮箱注册测试")
+    public void testEmail_SignUp() {
+        WebDriverWait wait = new WebDriverWait(iosDriver, 10);
+
+        // 点击 "邮箱登录"按钮
+        wait.until(ExpectedConditions.visibilityOfElementLocated(accountPage.EMAIL_LOGIN_BTN()));
+        iosDriver.findElement(accountPage.EMAIL_LOGIN_BTN()).click();
+
+        //输入邮箱
+        wait.until(ExpectedConditions.elementToBeClickable(accountPage.EMAIL_INPUT()));
+        iosDriver.findElement(accountPage.EMAIL_INPUT()).sendKeys(RandomStringUtils.randomAlphanumeric(6) + "@hi.com");
+
+        //点击 continue 进入下一页
+        wait.until(ExpectedConditions.elementToBeClickable(accountPage.CONTINUE_BUTTON()));
+        iosDriver.findElement(accountPage.CONTINUE_BUTTON()).click();
+
+        //输入密码
+        wait.until(ExpectedConditions.elementToBeClickable(accountPage.PASSWORD_INPUT()));
+        iosDriver.findElement(accountPage.PASSWORD_INPUT()).sendKeys(Utils.EMAIL_PASSWORD);
+
+        //点击 SignUp
+        wait.until(ExpectedConditions.elementToBeClickable(accountPage.SIGH_UP()));
+        iosDriver.findElement(accountPage.SIGH_UP()).click();
+
+        //跳过感兴趣的分类选择
+        wait.until(ExpectedConditions.presenceOfElementLocated(accountPage.CATEGORY_TITLE_ON_GUIDE()));
+        iosDriver.findElement(accountPage.SKIP_BTN_ON_GUIDE()).click();
+
+        //跳过专辑建议选择
+        wait.until(ExpectedConditions.presenceOfElementLocated(accountPage.SUGGESTIONS_TITLE_ON_GUIDE()));
+        iosDriver.findElement(accountPage.SKIP_BTN_ON_GUIDE()).click();
+
+        //accept 弹窗
+        wait.until(ExpectedConditions.alertIsPresent());
+        iosDriver.switchTo().alert().accept();
+
+        //断言测试成功
+        wait.until(ExpectedConditions.presenceOfElementLocated(discoverPage.TRENDING_BTN()));
+        Assert.assertTrue(iosDriver.findElement(discoverPage.DISCOVER_TAB_BTN()).isDisplayed(), "验证是否进入 discover");
+    }
+
+}
--- src/test/java/com/himalaya/uitest/ios/elements/AccountPage.java
+++ src/test/java/com/himalaya/uitest/ios/elements/AccountPage.java
@@ -1,6 +1,7 @@
 package com.***REMOVED***.uitest.ios.elements;
 
 
+import io.appium.java_client.MobileBy;
 import org.openqa.selenium.By;
 
 public class AccountPage {
@@ -12,19 +13,36 @@ public class AccountPage {
         return By.xpath("/XCUIElementTypeApplication[@name='***REMOVED***']/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTextField");
     }
 
-    public By CONTINUE_BUTTON(){
+    public By CONTINUE_BUTTON() {
         return By.xpath("//XCUIElementTypeButton[@name='Continue']");
     }
 
-    public By PASSWORD_INPUT(){
+    public By PASSWORD_INPUT() {
         return By.xpath("//XCUIElementTypeApplication[@name='***REMOVED***']/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeSecureTextField");
     }
 
-    public By LOGIN_BTN(){
+    public By LOGIN_BTN() {
         return By.xpath("//XCUIElementTypeButton[@name='Log In']");
     }
 
-    public By TOURIST_LOGIN_BTN(){
+    public By TOURIST_LOGIN_BTN() {
         return By.xpath("//XCUIElementTypeButton[@name='Skip For Now']");
     }
+
+    public By SKIP_BTN_ON_GUIDE() {
+        return MobileBy.AccessibilityId("Skip");
+    }
+
+    public By CATEGORY_TITLE_ON_GUIDE() {
+        return MobileBy.AccessibilityId("What categories are you interested in?");
+    }
+
+    public By SUGGESTIONS_TITLE_ON_GUIDE() {
+        return MobileBy.AccessibilityId("Need suggestions?");
+    }
+
+    public By SIGH_UP() {
+        return MobileBy.AccessibilityId("Sign Up");
+    }
+
 }
